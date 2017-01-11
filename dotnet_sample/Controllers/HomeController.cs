bcb9bfeb7424529f294f9403ed4b17133e67d8de
Niel Zeeman <niel@NIEL_YOGA>
2017-01-11 17:13:49 +0200

    added docker details page

--- dotnet_sample/Controllers/HomeController.cs
+++ dotnet_sample/Controllers/HomeController.cs
@@ -1,13 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication.Controllers
 {
     public class HomeController : Controller
     {
+        
+
         public IActionResult Index()
         {
             return View();
@@ -19,6 +24,25 @@ namespace WebApplication.Controllers
 
             return View();
         }
+
+         public IActionResult Docker()
+        {
+            
+            ViewData["MachineName"] = Environment.MachineName;
+            ViewData["OSArchitecture"] = RuntimeInformation.OSArchitecture;
+            ViewData["OSDescription"] = RuntimeInformation.OSDescription;
+            ViewData["FrameworkDescription"] = RuntimeInformation.FrameworkDescription;
+
+            var environmentVariables = new StringBuilder();
+            foreach( DictionaryEntry environmentVariable in Environment.GetEnvironmentVariables()){
+                environmentVariables.AppendFormat("{0} : {1}" + Environment.NewLine, environmentVariable.Key, environmentVariable.Value);
+            }
+
+           ViewData["EnvironmentVariables"] = environmentVariables.ToString();
+
+            return View();
+        }
+
 
         public IActionResult Contact()
         {
